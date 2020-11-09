48b0ec260680e48b446d2b38621fcdd6e279a409
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2020-11-09 15:58:33 +0100

    structure

--- client/abebox.py
+++ client/abebox.py
@@ -1,9 +1,19 @@
 import sys
 import os
-from fuse import FUSE, FuseOSError, Operations
-from Crypto.Cipher import AES
-from passthrough import Passthrough
 import secrets
+import tempfile
+import json
+from pathlib import Path
+
+from Crypto.Cipher import AES
+from charm.toolbox.pairinggroup import PairingGroup, GT
+from charm.core.engine.util import objectToBytes, bytesToObject
+from ABE.ac17 import AC17CPABE
+
+from fuse import FUSE, FuseOSError, Operations
+
+from passthrough import Passthrough
+
 
 import logging
 logging.basicConfig()
@@ -12,59 +22,150 @@ logger.info("started")
 
 class Abebox(Passthrough):
     def __init__(self, root):
-        self.symk = None
-        self.symnonce = None
-        self.SKEYLEN = 16
-        self.SNONCELEN = 8 
+        self.SYM_KEY_LEN = 16
+        self.SYM_NONCE_LEN = 8 
+
+        self.CHUNK_SIZE = 1024
+
+        #load abe key
+        self._load_abe_keys(str(Path.home()) + '/.abe_keys')
+
         super(Abebox, self).__init__(root)
 
-    def read(self, path, length, offset, fh):
-        print("Read with symk ", self.symk, "\t nonce ", self.nonce)
-        os.lseek(fh, offset, os.SEEK_SET)
-        ct = os.read(fh, length)
-        pt = self.cipher.decrypt(ct)
-        return pt
-    
+    # Utility functions
 
+    def _read_in_chunks(self, file_object, chunk_size=1024):
+        """Lazy function (generator) to read a file piece by piece.
+        Default chunk size: 1k."""
+        while True:
+            data = file_object.read(chunk_size)
+            if not data:
+                break
+            yield data
+
+    def _load_abe_keys(self, abe_keys_file):
+        """Load public and private abe keys"""
+
+        print("Loading abe keys from " + abe_keys_file)
+        with open(abe_keys_file, 'r') as f:
+            data = json.load(f)
+
+        self.pairing_group = PairingGroup('MNT224')
+        self.abe_pk = bytesToObject(bytes.fromhex(data['pk']), self.pairing_group)
+        self.abe_sk = bytesToObject(bytes.fromhex(data['sk']), self.pairing_group)
+        self.cpabe = AC17CPABE(self.pairing_group, 2)
+
+    def _create_meta(self):
+        """Create meta information about the file with keys
+        """
+        self.meta = {
+                'sym_key': secrets.token_hex(self.SYM_KEY_LEN),
+                'sym_nonce': secrets.token_hex(self.SYM_NONCE_LEN),
+                'policy': '(DEPT1 and TEAM1)', # hardcoded - TBD
+        }
+        return self.meta
+        
+
+    def _load_meta(self, metafile):
+        """Fetch, decrypt and decode the metafile containing keys.
+        Create if it does not exist.
+        """
+        try:
+            print("try to open metafile: " + metafile)
+            with open(metafile) as f:
+                enc_meta = json.load(f)
+        except FileNotFoundError:
+            print("Metafile not found")
+            # TBD: try to recover?
+
+        enc_sym_key = bytearray.fromhex(enc_meta['enc_sym_key'])
+        # decrypt the sym key
+        sym_key = cpabe.decrypt(self.abe_pk, enc_sym_key, self.abe_sk)
+
+        # create a symmetric cypher
+        self.sym_cipher = AES.new(self.sym_key, AES.MODE_CTR, nonce=bytearray.fromhex(self.finfo['nonce']))
+
+        self.meta = {
+                'sym_key': enc_meta,
+                'nonce': bytearray.fromhex(enc_meta['nonce']),
+                'policy': '(DEPT1 and TEAM1)', # hardcoded - TBD
+        }
+        return self.finfo
+
+    def _dump_meta(self, metafile):
+        """Dump the meta information on the meta file
+        """
+        enc_sym_key = self.cpabe.encrypt(self.abe_pk, self.meta['sym_key'], self.meta['policy'])
+        enc_meta = {
+            'sym_key': enc_sym_key,        
+            'nonce': self.meta['nonce'],        
+            'policy': '(DEPT1 and TEAM1)', # hardcoded - TBD
+        }
+        json.dump(metafile, enc_meta)
+        return enc_meta
+
+    # Fuse callbacks
+
+    def read(self, path, length, offset, fh):
+        return super(Abebox, self).read(path, length, offset, self.temp_fp)
+    
     def write(self, path, buf, offset, fh):
-        print("Write with symk ", self.symk, "\t nonce ", self.nonce)
-        os.lseek(fh, offset, os.SEEK_SET)
-        ct = self.cipher.encrypt(buf)
-        #return os.write(fh, buf)
-        return os.write(fh, ct)
+        return super(Abebox, self).write(path, buf, offset, self.temp_fp)
 
     def open(self, path, flags):
-        print("open")
-        with open(self._full_path(path) + '.meta', 'r') as f:
-            buf = f.read()
-            self.symk = bytearray.fromhex(buf[:self.SKEYLEN*2])
-            self.nonce = bytearray.fromhex(buf[self.SKEYLEN*2:])
-            print("Symmetric Key: ", self.symk, "\t nonce: ", self.nonce)
-            self.cipher = AES.new(self.symk, AES.MODE_CTR, nonce=self.nonce)
-        return super(Abebox, self).open(path, flags)
+        print("Opening file ", path)
+        self.is_new = False
+
+        # load meta information
+        self.dirname, self.filename = os.path.split(os.path.dirname(self._full_path(path))) 
+        print("dirname {} - filename {}", self.dirname, self.filename)
+        self._load_meta(self.dirname + '/.abebox/' + self.filename)
+
+        # open a temporary file
+        self.temp_fp = tempfile.TemporaryFile() # could be more secure with mkstemp()
+
+        # TBD reverse AONT and remove re-encryption
+
+        # decrypt the file with the sym key and write it on the temporary file
+        with open(self._full_path(path), 'r') as enc_fp:
+            for chunk in self._read_in_chunks(enc_fp, self.CHUNK_SIZE):
+                self.temp_fp.write(self.cipher.decrypt(chunk))
+
+        return self.temp_fp.fileno()
+        #return super(Abebox, self).open(path, flags)
         
     def create(self, path, mode, fi=None):
         print("Creating file ", path)
-        symk = secrets.token_hex(self.SKEYLEN)
-        nonce = secrets.token_hex(self.SNONCELEN)
+        # open a temporary file
+        self.temp_fp = tempfile.TemporaryFile() # could be more secure with mkstemp() 
 
-        with open(self._full_path(path) + '.meta', 'w') as f:
-            f.write(symk)
-            f.write(nonce)
+        self.dirname, self.filename = os.path.split(os.path.dirname(self._full_path(path))) 
+        self.is_new = True
 
-        self.symk = bytearray.fromhex(symk)
-        self.nonce = bytearray.fromhex(nonce)
-        self.cipher = AES.new(self.symk, AES.MODE_CTR, nonce=self.nonce)
+        self._create_meta()
 
-        return super(Abebox, self).create(path, mode, fi)
+        return self.temp_fp.fileno()
+
 
     def release(self, path, fh):
-        print("release")
-        return super(Abebox, self).release(path, fh)
+        print("Releasing file ", path)
+        # pour the temp file in the dest folder
+        self.temp_fp.seek(0)
+        with open(self._full_path(path), 'w') as enc_fp:
+            for chunk in self._read_in_chunks(self.temp_fp, self.CHUNK_SIZE):
+                enc_fp.write(self.cipher.encrypt(chunk))
+
+        self._dump_meta(self.dirname + '/.abebox/' + self.filename)
+
+        self._create_meta(self.dirname + '/.abebox/' + self.filename)
+
+        # ret = os.close(self.temp_fp) # temporary files are automatically deleted
 
 
 def main(mountpoint, root):
     FUSE(Abebox(root), mountpoint, nothreads=True, foreground=True)
 
 if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Syntax: " + sys.argv[0] + " basedir mountdir")
     main(sys.argv[2], sys.argv[1])
---
+++ client/create_abe_msk.py
@@ -0,0 +1,64 @@
+"""
+Utility file: create a test msk and private key
+"""
+
+from charm.toolbox.pairinggroup import PairingGroup, GT
+from charm.core.engine.util import objectToBytes, bytesToObject
+
+#from bsw07 import BSW07
+from ABE.ac17 import AC17CPABE
+
+from pathlib import Path
+import json
+
+
+def main():
+    # instantiate a bilinear pairing map
+    # 'MNT224' represents an asymmetric curve with 224-bit base field
+    pairing_group = PairingGroup('MNT224')
+
+    # CP-ABE under DLIN (2-linear)
+    cpabe = AC17CPABE(pairing_group, 2)
+
+    # run the set up
+    (pk, msk) = cpabe.setup()
+
+    # generate a user key
+    user_attr_list = ['DEPT1', 'TEAM1']
+    user_key = cpabe.keygen(pk, msk, user_attr_list)
+
+    # generate a ciphertext
+    #policy_str = '((PATIENT and SMART-1032702) or (PRACTITIONER and SMART-PRACTITIONER-72004454))'
+    print("---------- PK ----------")
+    print(pk)
+
+    print("---------- MSK (needed only for debug)----------")
+    print(msk)
+
+    print("---------- SK ----------")
+    print(user_key)
+
+    data = {
+        'pk': objectToBytes(pk, pairing_group).hex(),
+        #'msk': msk,
+        'sk': objectToBytes(user_key, pairing_group).hex(),
+    }
+
+    #print(json.dumps(data))
+
+    ######
+    abe_keys_file = str(Path.home()) + '/.abe_keys'
+
+    answer = input("Should I write them on your " + abe_keys_file + "[Y/N]? ").lower()
+    if answer == 'y':
+        with open(abe_keys_file, 'w') as f:
+            json.dump(data, f)
+        print("Done")
+    else:
+        print("Could we at least be friend?")
+
+
+
+if __name__ == "__main__":
+    main()
+
